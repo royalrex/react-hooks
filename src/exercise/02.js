d378900e89d3503c14e3dcc15e561d54043822ac
sebbe <sebbe@rxdesign.dk>
2023-08-10 08:43:39 +0200

    hooks: 1+2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,31 +3,74 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
+function useLocalStorageState(key, initialValue) {
+  const getInitialValue = () => {
+    console.log("read")
+    const lsValue = localStorage.getItem(key)
+    if (lsValue) {
+      if (typeof initialValue === 'string') return lsValue
+
+      return JSON.parse(lsValue)
+    }
+
+    return initialValue
+  }
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const [value, setValue] =  React.useState(() => getInitialValue())
+  React.useEffect(() => {
+    // useEffect gets called after each render cycle, only limited by the dependency array. Dependency is more like run only if logic, and not triggers.
+    if (typeof initialValue === 'string') {
+      localStorage.setItem(key, value)
+    } else {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
+    console.log("set")
+  }, [key, value])
+
+  return [value, setValue]
+}
+
+function Greeting({initialName = ''}) {
+  console.log("render")
+  // const getInitialName = () => {
+  //   console.log("read")
+  //   const lsName = localStorage.getItem('name')
+  //   if (lsName) return lsName
+  //
+  //   return initialName
+  // }
+  // 🐨 initialize the state to the value from localStorage
+  // 💰 window.localStorage.getItem('name') ?? initialName
+  const [obj, setName] = useLocalStorageState('name', {initialName, name:initialName})
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
 
   function handleChange(event) {
-    setName(event.target.value)
+    setName((prev) => {
+      return {...prev, name:event.target.value}
+    })
   }
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={obj.name} onChange={handleChange} id="name" autoComplete="off" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {obj.name ? <strong>Hello {obj.name}</strong> : 'Please type your name'}
     </div>
   )
 }
 
 function App() {
-  return <Greeting />
+  const [number, setNumber] = useLocalStorageState('counter', 0)
+console.log(typeof number, number)
+  return <>
+    <button onClick={() => setNumber(number+1)}>{number}</button>
+    <Greeting />
+  </>
 }
 
 export default App
